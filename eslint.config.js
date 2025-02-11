5e83941adc34c5a6b867a9a589963f20f4d55b69
Tori Schuyler <tori.schuyler@icloud.com>
2025-02-11 07:24:22 -0500

    chore: set up ESLint with config and npm scripts

---
+++ eslint.config.js
@@ -0,0 +1,6 @@
+export default {
+    "rules": {},
+    // empty rules object means "use defaults" 
+    "ignores": ["node_modules/"]
+    // ignoring node_modules prevents ESLint from trying to check all your dependencies
+}
