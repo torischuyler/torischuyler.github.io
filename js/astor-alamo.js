c3803d9f20b45ef35a27f62afcc335539508add2
Tori Schuyler <tori.schuyler@icloud.com>
2025-07-31 17:18:51 -0500

    style(css, js): Improve cube prompt styling and camera positioning in 1 CSS file and 1 JS file

--- js/astor-alamo.js
+++ js/astor-alamo.js
@@ -4,6 +4,7 @@ const scene = new THREE.Scene();
 // Create a camera (like your eyes viewing the scene)
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 camera.position.z = 5; // Move the camera back so we can see the cube
+camera.position.y = -1;
 
 // Create the renderer (draws everything to the screen)
 const renderer = new THREE.WebGLRenderer();
